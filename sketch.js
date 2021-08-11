4525ccfed6b4f0b08b82f8aa9e9346c32ea323e7
anikajkrishna <86082632+anikajkrishna@users.noreply.github.com>
2021-08-10 20:21:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function preload(){
+var seaImg = loadImage("sea.png");
+var ship1Img = loadAnimation("ship-1.png");
+var ship2Img = loadAnimation("ship-2.png");
+var ship3Img = loadAnimation("ship-1.png");
+var ship4Img = loadAnimation("ship-2.png");
+seaImg.addImage = (seaImg);
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+}
